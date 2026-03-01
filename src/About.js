01513b252e0f0ee9240dbcc26e2f2d2c20fa48f1
gitpalni <ravinder.palni@gmail.com>
2026-03-01 19:12:58 +0530

    change URL for resume
    change URL for resume

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <Navbar/> 
       <img className='img_x' src={img1} alt="" />
       <span className = "anchor">
-        <a href = "https://drive.google.com/file/d/1OsPeNtAEUVLA06cWRE4GYLraCd3d0G67/view?usp=drive_link" target = "_blank">Resume</a><span className='t'> | </span>
+        <a href = "./Shambhavi Palni-CV-v2602.pdf" target = "_blank">Resume</a><span className='t'> | </span>
         <a href = "mailto:shambhavipalnicontact@gmail.com" target='_blank'>shambhavipalnicontact@gmail.com</a>
       </span>
     </div>
